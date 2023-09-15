9b44e09be82b0588d70d8c62e59c554ad89d9465
Daniil Batiuk <daniil.batiuk@student.tuke.sk>
2023-09-15 13:36:18 +0300

    Lab2

---
+++ lab_2_Batiuk/lab_2_Batiuk.py
@@ -0,0 +1,22 @@
+print("    *");
+print("   * *");
+print("  *   *");
+print(" *     *");
+print("***   ***");
+print("  *   *");
+print("  *   *");
+print("  *****");
+
+
+print("I'm student");
+
+print("\"I'm\"\n\"\"learning\"\"\n\"\"Python\"\"");
+
+print(int('500', 8));
+
+print(int('777', 16));
+
+print("Programming***Essentials***in...Python")
+
+
+print(3 ** 3 ** 4);
